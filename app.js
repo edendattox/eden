2454dd971eddc8769f657fa5dab0dee538de0456
edendattox <74775804+edendattox@users.noreply.github.com>
2021-01-06 19:36:50 +0530

    damnson

--- app.js
+++ app.js
@@ -4,32 +4,32 @@ const ejs = require("ejs");
 
 const app = express();
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/img', express.static(__dirname + 'public/img'));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
 
-app.get("", function(req, res) {
-    res.render('home');
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get("/about", function(req, res) {
-    res.render("about");
-})
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 
-app.get("/contact", function(req, res) {
-   res.render("contact");
- });
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
 
- app.get("/projects", function(req, res) {
-    res.render("projects");
-  });
+app.get("/projects", function (req, res) {
+  res.render("projects");
+});
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
